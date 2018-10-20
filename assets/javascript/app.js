19379f307752312188708086e8c32083b10e2ab3
That Lisa <thatlisajones@gmail.com>
2018-10-20 02:01:29 -0600

    New images for answer pages. Reduced timer length. Style adjustments.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,64 +3,64 @@ $(document).ready(function () {
     //Load questions, answer choices, answer position, and image/video links in arrays of options.
     var options = [
         {
-            question: "Vine was a short-form video hosting service that allowed users to upload short, looping videos. Many makers of Vine videos regarded the strict time limit as a creative challenge. How long could a Vine video be?", 
+            question: "Vine was a short-form video hosting service that allowed users to upload short, looping videos. How long could a Vine video be?", 
             choice: ["10 seconds", "7 seconds", "6 seconds", "8 seconds"],
             answer: 2,
-            photo: "assets/images/vine-videos/revenge-vine-sq.mp4"
+            photo: "assets/images/vine.jpg"
          },
          {
-             question: "What is 'the vine that ate the South'? It's a fast-spreading, invasive vine that was introduced from Japan into the United States in 1876. Initially, it was used as a ground-cover plant to control soil erosion.", 
+             question: "What is 'the vine that ate the South'? It was introduced into the U.S. to control soil erosion.", 
             choice: ["Prickle Brier", "Patricia", "Leafy Spurge", "Kudzu"],
             answer: 3,
-            photo: "assets/images/vine-videos/patricia-vine-sq.mp4"
+            photo: "assets/images/kudzu.jpg"
          }, 
          {
-             question: "A portion of which California Interstate Highway is known as 'the Grapevine'? In winter, this stretch of highway is often closed, stranding motorists.", 
+             question: "A portion of which California Interstate Highway is known as 'the Grapevine'?", 
             choice: ["405", "5", "101", "605"],
             answer: 1,
-            photo: "assets/images/vine-videos/roadwork-vine-sq.mp4"
+            photo: "assets/images/I5.jpg"
         }, 
         {
-            question: "Singer Marvin Gaye scored a big hit with the song 'I Heard It Through the Grapevine' in 1968. Who recorded and released the song one year earlier?", 
+            question: "Marvin Gaye had a hit with 'I Heard It Through the Grapevine' in 1968. Who recorded and released the song one year earlier?", 
             choice: ["Gladys Knight & the Pips", "Creedence Clearwater Revival", "The California Raisins", "Stevie Wonder"],
             answer: 0,
-            photo: "assets/images/vine-videos/roses-vine-sq.mp4"
+            photo: "assets/images/gkpips.jpg"
         }, 
         {
             question: "Vineyard Vines is a clothing company that sells 'preppy,' 'southern-styled' attire. What animal appears on the company's logo?", 
             choice: ["Sheep", "Alligator", "Emu", "Whale"],
             answer: 3,
-            photo: "assets/images/vine-videos/adam-vine-sq.mp4"
+            photo: "assets/images/vinvines.jpg"
         }, 
         {
-            question: "Which of these regions of France is NOT known for its wine? While it's not an official wine region, it is known for its cider and calvados.", 
+            question: "Which of these regions of France is NOT known for its wine, but for its cider and calvados?", 
             choice: ["Bordeaux", "Normandy", "Languedoc", "Loire"],
             answer: 1,
-            photo: "assets/images/vine-videos/matrix-vine-sq.mp4"
+            photo: "assets/images/normandy.jpg"
         }, 
         {
             question: "Which dessert wine is produced from grapes that have been frozen while still on the vine? Canada and Germany are the world's largest producers.", 
             choice: ["Ice Wine", "Eiswein", "Vin de glace", "All of the above"],
             answer: 3,
-            photo: "assets/images/vine-videos/ice-vine-sq.mp4"
+            photo: "assets/images/ice.jpg"
         }, 
         {
             question: "Eight schools make up the Ivy League athletic conference. Which of these is an Ivy League school?", 
             choice: ["Claremont College", "University of Pennsylvania", "Stanford", "Colgate University"],
             answer: 1,
-            photo: "assets/images/vine-videos/tball-vine-sq.mp4"
+            photo: "assets/images/penn.jpg"
         }, 
         {
             question: "Which popular food grows on a shrub or bush as well as on a vine?", 
             choice: ["Grape", "Tomato", "Cherry", "Cashew"],
             answer: 1,
-            photo: "assets/images/vine-videos/fbi-vine-sq.mp4"
+            photo: "assets/images/tomato.jpg"
         }, 
         {
             question: "Which plant contains a toxic oil called urushiol, which can cause a painful, itchy rash if it comes in contact with your skin?", 
             choice: ["Poison Ivy", "Poison Oak", "Poison Sumac", "All of the above"],
             answer: 3,
-            photo: "assets/images/vine-videos/fbi-vine-sq.mp4"
+            photo: "assets/images/urushiol.jpg"
         }];
 
     //Define variables for game functions 
@@ -68,7 +68,7 @@ $(document).ready(function () {
     var correctCount = 0;
     var wrongCount = 0;
     var unanswerCount = 0;
-    var timer = 20;
+    var timer = 10;
     var intervalId;
     var userGuess ="";
     var running = false;
@@ -167,7 +167,7 @@ function hidepicture () {
 
     var hidpic = setTimeout(function() {
         $("#answerblock").empty();
-        timer= 20;
+        timer= 10;
 
     //Display final score when complete question array is answered
     if ((wrongCount + correctCount + unanswerCount) === qCount) {
